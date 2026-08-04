68594bd4caa37047cea5efc38e5ff770011d1808
lycawackerbauer-dev <lyca.wackerbauer@icloud.com>
2026-08-04 22:20:54 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,35 @@
-const ktoName = "Girokonto";
-const sparkto = "Sparkonto";
-const ktoStand = 1000
-const spktoStand = 15000
+const konten = [
+  {
+    name: "Girokonto",
+    kontostand: 1000,
+    typ: "Zahlungsmittel"
+  },
+
+  {
+    name: "Kreditkarte",
+    kontostand: 1500,
+    typ: "Zahlungsmittel"
+  },
+
+  {
+    name: "Bargeld",
+    kontostand: 50,
+    typ: "Zahlungsmittel"
+  },
+  
+  {
+    name: "Sparkonto",
+    kontostand: 15000,
+    typ: "Sparen"
+  },
+
+  {
+    name: "Sparen Bargeld",
+    kontostand: 70.95,
+    typ: "Sparen"
+  }
+];
+
+
+  
+
